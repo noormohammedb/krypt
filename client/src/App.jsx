9aa70ecf976684bf82a3ed43b3466a6411037596
Noor Mohammed <mail@noormohammed.tech>
2021-12-29 13:51:55 +0530

    Frontend Project Structured For Development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import {
+  Footer,
+  Loader,
+  Navbar,
+  Services,
+  Transactions,
+  Welcome,
+} from "./components";
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Loader />
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
